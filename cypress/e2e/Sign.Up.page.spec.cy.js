0e6f7b61806b4f1a288cd707a188a699e9a24ca3
Moshkovskiy Volodomyr <moshkovskiy.v.v@Gmail.com>
2023-08-31 17:38:55 +0200

    Optimaze some test, add new and apdata linter

--- cypress/e2e/Sign.Up.page.spec.cy.js
+++ cypress/e2e/Sign.Up.page.spec.cy.js
@@ -13,18 +13,18 @@ it('should register user', () => {
     
   const {email, username, password} = generateUser()
 
-  cy.get('h1')
-    .should('contain.text', 'Sign up');
-  cy.findByPlaceholder('Username')
-    .type(username);
-  cy.findByPlaceholder('Email')
-    .type(email);
-  cy.findByPlaceholder('Password')
-    .type(password);
-  cy.get('.btn')
-    .click();
-  cy.get('.swal-text')
-    .should('contain.text', 'Your registration was successful!');
+  cy.get('h1').should('contain.text', 'Sign up');
+
+  cy.findByPlaceholder('Username').type(username);
+
+  cy.findByPlaceholder('Email').type(email);
+
+  cy.findByPlaceholder('Password').type(password);
+
+  cy.get('.btn').click();
+
+  cy.get('.swal-text').should('contain.text', 'Your registration was successful!');
+
   // cy.assertPageUrl('/')
 
 });
@@ -33,16 +33,16 @@ it('Can`t register with password without number', () => {
     
   const {email, username, password} = generateUser()
 
-  cy.get('h1')
-    .should('contain.text', 'Sign up');
-  cy.findByPlaceholder('Username')
-    .type(username);
-  cy.findByPlaceholder('Email')
-    .type(email);
-  cy.findByPlaceholder('Password')
-    .type('Qwertyqwe');
-  cy.get('.btn')
-    .click();
+  cy.get('h1').should('contain.text', 'Sign up');
+
+  cy.findByPlaceholder('Username').type(username);
+
+  cy.findByPlaceholder('Email').type(email);
+
+  cy.findByPlaceholder('Password').type('Qwertyqwe');
+
+  cy.get('.btn').click();
+
   cy.get('.swal-text')
     .should('contain.text', 'Password must be 8 characters long and include 1 number, 1 uppercase letter, and 1 lowercase letter.');
   });
@@ -51,16 +51,16 @@ it('Can`t register without 1 uppercase letter', () => {
     
   const {email, username, password} = generateUser()
 
-  cy.get('h1')
-    .should('contain.text', 'Sign up');
-  cy.findByPlaceholder('Username')
-    .type(username);
-  cy.findByPlaceholder('Email')
-    .type(email);
-  cy.findByPlaceholder('Password')
-  .type('qwerty123');
-  cy.get('.btn')
-    .click();
+  cy.get('h1').should('contain.text', 'Sign up');
+
+  cy.findByPlaceholder('Username').type(username);
+
+  cy.findByPlaceholder('Email').type(email);
+
+  cy.findByPlaceholder('Password').type('qwerty123');
+
+  cy.get('.btn').click();
+
   cy.get('.swal-text')
     .should('contain.text', 'Password must be 8 characters long and include 1 number, 1 uppercase letter, and 1 lowercase letter.');
 });
@@ -69,16 +69,16 @@ it('Can`t register with short password', () => {
 
   const {email, username, password} = generateUser()
 
-  cy.get('h1')
-    .should('contain.text', 'Sign up');
-  cy.findByPlaceholder('Username')
-    .type(username);
-  cy.findByPlaceholder('Email')
-    .type(email);
-  cy.findByPlaceholder('Password')
-  .type('123qW');
-  cy.get('.btn')
-    .click();
+  cy.get('h1').should('contain.text', 'Sign up');
+
+  cy.findByPlaceholder('Username').type(username);
+
+  cy.findByPlaceholder('Email').type(email);
+
+  cy.findByPlaceholder('Password').type('123qW');
+
+  cy.get('.btn').click();
+
   cy.get('.swal-text')
     .should('contain.text', 'Password must be 8 characters long and include 1 number, 1 uppercase letter, and 1 lowercase letter.');
 });
@@ -87,16 +87,16 @@ it('Can`t register without 1 lowercase letter', () => {
     
   const {email, username, password} = generateUser()
 
-  cy.get('h1')
-    .should('contain.text', 'Sign up');
-  cy.findByPlaceholder('Username')
-    .type(username);
-  cy.findByPlaceholder('Email')
-    .type(email);
-  cy.findByPlaceholder('Password')
-    .type('QWER1234543');
-  cy.get('.btn')
-    .click();
+  cy.get('h1').should('contain.text', 'Sign up');
+
+  cy.findByPlaceholder('Username').type(username);
+
+  cy.findByPlaceholder('Email').type(email);
+
+  cy.findByPlaceholder('Password').type('QWER1234543');
+
+  cy.get('.btn').click();
+
   cy.get('.swal-text')
     .should('contain.text', 'Password must be 8 characters long and include 1 number, 1 uppercase letter, and 1 lowercase letter.');
 });
@@ -105,37 +105,93 @@ it('Should not allow with 2 simle Email', () => {
     
   cy.registerNewUser().then(({email, username, password}) => {
 
-    cy.findByPlaceholder('Username')
-    .type(username);
-  cy.findByPlaceholder('Email')
-    .type(email);
-  cy.findByPlaceholder('Password')
-    .type(password);
-  cy.get('.btn')
-    .click();
-  cy.get('.swal-text')
-    .should('contain.text', 'Email already taken.');
+    cy.findByPlaceholder('Username').type(username);
+
+  cy.findByPlaceholder('Email').type(email);
+
+  cy.findByPlaceholder('Password').type(password);
+
+  cy.get('.btn').click();
+
+  cy.get('.swal-text').should('contain.text', 'Email already taken.');
+
   })
 
 
-  })
+})
 
 it('can`t register user with invalid email', () => {
     
 const {email, username, password} = generateUser()
 
-  cy.get('h1')
-    .should('contain.text', 'Sign up');
-  cy.get('[placeholder=Username]')
-    .type(username);
-  cy.get('[placeholder=Email]')
-    .type('email.com.ua');
-  cy.get('[placeholder=Password]')
-    .type(password);
-  cy.get('.btn')
-    .click();
+  cy.get('h1').should('contain.text', 'Sign up');
+
+  cy.get('[placeholder=Username]').type(username);
+
+  cy.get('[placeholder=Email]').type('email.com.ua');
+
+  cy.get('[placeholder=Password]').type(password);
+
+  cy.get('.btn').click();
+
   cy.get('.swal-text')
     .should('contain.text', 'Email must be a valid email.');
 });
+
+it('should not register whithout email', () => {
+
+const {username, password} = generateUser()
+
+  cy.findByPlaceholder('Username').type(username);
+
+  cy.findByPlaceholder('Password').type('password');
+
+  cy.get('.btn').click();
+
+  cy.get('.swal-text').should('contain.text', 'Email field required.');
+ 
+});
+
+it('should not register whithout email', () => {
+
+const {email, password} = generateUser()
+  
+  cy.findByPlaceholder('Email').type(email);
+
+  cy.findByPlaceholder('Password').type('password');
+
+  cy.get('.btn').click();
+
+  cy.get('.swal-text').should('contain.text', 'Username field required.');
+   
+});
+
+it('should not register whithout email', () => {
+
+const {email, username} = generateUser()
+    
+  cy.findByPlaceholder('Email').type(email);
+
+  cy.findByPlaceholder('Username').type('username');
+
+  cy.get('.btn').click();
+
+  cy.get('.swal-text').should('contain.text', 'Password field required.');
+     
+});
+
+it('User can use Tab and Enter 2 work with form', () => {
+
+  const {email, username} = generateUser()
+      
+    cy.findByPlaceholder('Email').type(email);
+  
+    cy.findByPlaceholder('Username').type('username');
+  
+    cy.get('.btn').click();
+  
+    cy.get('.swal-text').should('contain.text', 'Password field required.');
+       
+  });
 
 });    
--- cypress/e2e/home.page.spec.cy.js
+++ cypress/e2e/home.page.spec.cy.js
@@ -4,27 +4,26 @@ beforeEach(() => {
   cy.visit('/');
 });
 
-describe('My home page', () => {
+describe('It should have a title', () => {
   it('should have all parts', () => {
-    // Проверяем что мы перешли на страницу 
     cy.get('h1').should('contain.text', 'conduit');
-    
-    // Проверяем что есть Global Feed 
+  });
+
+  it('should have Global Feed', () => {
     cy.contains('a', 'Global Feed').should('be.visible');
-    
-    // Проверяем что в сайдбаре есть указанный текст
+  });
+
+  it('should have Popular tags', () => {
     cy.contains('.sidebar', 'Popular Tags').should('be.visible');
   });
 
   it('should click on Sign in', () => {
-    // Проверяем что можно перейти на страницу логина
     cy.contains('a', 'Sign in').click();
     cy.url().should('include', '/login');
     cy.get('h1').should('contain.text', 'Sign in');
   });
 
   it('should click on Sign up', () => {
-    // Проверяем что можно перейти на страницу регистрации
     cy.contains('a', 'Sign up').click();
     cy.url().should('include', '/register');
     cy.get('h1').should('contain.text', 'Sign up');
